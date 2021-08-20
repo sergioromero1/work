2a9092ff1af4741b7f223c904eb0fb651e7e318f
Sergio <sar9206@gmail.com>
2021-08-19 20:08:55 -0500

    jsondecode error

--- decoradores/loop.py
+++ decoradores/loop.py
@@ -1,11 +1,12 @@
 import time
+from json.decoder import JSONDecodeError
 
 def loop(func):
     def wrapper(self):
         while True:
             try:
                 func(self)
-            except (ValueError, ConnectionError):
+            except (ValueError, ConnectionError, JSONDecodeError):
                 time.sleep(20)
                 func(self)
     return wrapper
