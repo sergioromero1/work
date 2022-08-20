e18bb861e807482251924502fd455585c5905d7b
Sergio <sar9206@gmail.com>
2022-08-20 15:15:53 -0500

    settings

---
+++ settings/settings.py
@@ -0,0 +1,12 @@
+import environ
+
+env = environ.Env()
+
+BOT_TOKEN = env('BOT_TOKEN')
+CR_KEY = env('CR_KEY')
+CR_SECRET = env('CR_SECRET')
+MX_KEY = env('MX_KEY')
+MX_SECRET = env('MX_SECRET')
+COMPRA_KEY = env('COMPRA_KEY')
+COMPRA_SECRET = env('COMPRA_SECRET')
+
