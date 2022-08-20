9796ecc3299291ad27aba27d6522ee56f8e5d043
Sergio <sar9206@gmail.com>
2022-08-20 14:44:10 -0500

    cambios en aviso de prendido de compra y venta

--- agentes/caller.py
+++ agentes/caller.py
@@ -1,4 +1,7 @@
 from .conectar import Connection
+from decoradores.loop import loop
+import time
+import requests
 
 class Caller:
 
@@ -23,5 +26,63 @@ class Caller:
         """Retorna los atributos solicitados como una tupla"""
 
         return (self.__dict__[nombre] for nombre in nombres)
+
+class Notificador(Caller):
+
+    def __init__(self, ad_id,key, secret, parametros, bot_token,enviar_mensaje,verificador,administrador, sleep_time,tipo):
+        super().__init__(ad_id,key, secret, parametros)
+        self.bot_token = bot_token
+        self.enviar_mensaje = enviar_mensaje
+        self.verificador = verificador
+        self.administrador = administrador
+        self.sleep_time = sleep_time
+        self.tipo =tipo
+    
+    def is_active(self,conn):
+
+        ad_id, = self.get_atributos("ad_id")
+        response = conn.call(method='GET', url= f'/api/ad-get/', params={'ads': {ad_id}})
+        ad = response.json()['data']['ad_list'][0]['data']
+        visible = ad['visible']
+
+        return visible
+
+    def sendtext(self, receptor, bot_message):
+
+        bot_token, enviar_mensaje = self.get_atributos("bot_token", "enviar_mensaje")
+        if enviar_mensaje:
+            send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + receptor + '&parse_mode=Markdown&text=' + bot_message
+            response = requests.post(send_text)
+            
+            return response.json()['ok']
+
+    @loop
+    def iniciar(self):
+
+        sleep_time = int(self.sleep_time)
+
+        while True:
+            self.activar()
+            time.sleep(sleep_time)
+
+    def activar(self):
+        administrador, tipo, verificador, enviar_mensaje = self.get_atributos("administrador", "tipo", "verificador", "enviar_mensaje")
+        conn = self.conectar()
+        se_activo = False
+        while True:
+            print('Revisando estado....', flush=True)
+            active = self.is_active(conn)
+            
+            if active and not se_activo:
+                se_activo = True
+                if enviar_mensaje:
+                    self.sendtext(administrador,f'El comercio de {tipo} de CR está PRENDIDO -- PRENDIDO')
+                    self.sendtext(verificador,f'El comercio de {tipo} de CR está PRENDIDO -- PRENDIDO')
+
+            if not active and se_activo:
+                se_activo = False
+                if enviar_mensaje:
+                    self.sendtext(administrador,f'El comercio de {tipo} de CR está APAGADO')
+                    self.sendtext(verificador,f'El comercio de {tipo} de CR está APAGADO')
 
     
---
+++ prende_compra.py
@@ -0,0 +1,24 @@
+from agentes.caller import Notificador
+from settings.settings import BOT_TOKEN, CR_KEY, CR_SECRET
+
+if __name__ == "__main__":
+
+    currency = 'CRC'
+    ad_id = '1397853'
+    key = CR_KEY
+    secret = CR_SECRET
+    verificador = '1526093626' #kozel
+    verificador2 = ''
+    administrador = '333685986' #sergio
+    parametros = {}
+    enviar_mensaje = True
+    bot_token = BOT_TOKEN
+    sleep_time = 10
+    tipo = 'compra'
+    notif = Notificador(ad_id,key, secret, parametros, bot_token,enviar_mensaje,verificador,administrador, sleep_time,tipo)
+    notif.iniciar()
+
+
+
+
+
