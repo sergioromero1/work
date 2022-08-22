37e73094e81569785dbdedd33e6b4b20a3b32baf
Sergio <sar9206@gmail.com>
2022-08-22 15:18:31 -0500

    cambios por bug de max_amount_available

--- agentes/vendedor.py
+++ agentes/vendedor.py
@@ -246,7 +246,7 @@ class Vendedor:
                 inside_dict['name'] = str(ad[position]['data']['profile']['username'])
                 inside_dict['price'] = float(ad[position]['data']['temp_price'])
                 inside_dict['min_amount'] = float(ad[position]['data']['min_amount']) if ad[position]['data']['min_amount'] is not None else 0
-                inside_dict['max_amount'] = float(ad[position]['data']['max_amount_available'])
+                inside_dict['max_amount'] = float(ad[position]['data']['max_amount_available']) if ad[position]['data']['max_amount_available'] is not None else (inside_dict['min_amount'] + 1) * 10
 
                 position += 1
 
